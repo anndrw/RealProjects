d1c3f23c0fa55763f97c23b0e6a818981048a3be
anndrw <andrwpricope@gmail.com>
2025-08-03 03:42:35 +0300

    Adaugate imbunatatiri la lift ce tin de functionare (kg, persoane, disponibilitate, revizie).

--- Lift/src/ro/real/clase/Lift.java
+++ Lift/src/ro/real/clase/Lift.java
@@ -1,12 +1,19 @@
 package ro.real.clase;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 public class Lift {
     private int etajCurent;
     private LiftState stare;
+    private boolean disponibil = true;
+    private LocalDate dataRevizie;
 
-    public Lift() {
+
+    public Lift(LocalDate dataRevizie) {
         this.etajCurent = 0;
         this.stare = new IdleState();
+        this.dataRevizie = dataRevizie;
     }
 
     public int getEtajCurent() {
@@ -25,17 +32,72 @@ public class Lift {
         this.stare = stare;
     }
 
+
+    public LocalDate getDataRevizie() {
+        return dataRevizie;
+    }
+
+    public void setDataRevizie(LocalDate dataRevizie) {
+        this.dataRevizie = dataRevizie;
+    }
+
     public void urcaEtaj(int floor){
-        System.out.println("Liftul urca de la etajul " + etajCurent + " la etajul " + floor);
-        this.etajCurent = floor;
+        if(!disponibil){
+            System.out.println("Momentan indisponibil.");
+        } else {
+            System.out.println("Liftul urca de la etajul " + etajCurent + " la etajul " + floor);
+            this.etajCurent = floor;
+        }
+
     }
 
     public void coboaraEtaj(int floor){
-        System.out.println("Liftul coboara de la etajul " + etajCurent + " la etajul " + floor);
-        this.etajCurent = floor;
+        if(!disponibil) {
+            System.out.println("Momentan indisponibil.");
+
+        } else {
+            System.out.println("Liftul coboara de la etajul " + etajCurent + " la etajul " + floor);
+            this.etajCurent = floor;
+        }
+
+    }
+
+
+    public void setDisponibil(boolean disponibil) {
+        this.disponibil = disponibil;
+    }
+
+    public void pornireLiftPersoane(int nrPersoane){
+        if(nrPersoane > 5) {
+            System.out.println("Liftul nu poate functiona daca sunt mai mult de 5 persoane in acesta.");
+            setDisponibil(false);
+        } else {
+            System.out.println("Liftul cu " + nrPersoane + " poate pleca!");
+        }
+    }
+
+    public void pornireLiftKilograme(int kg){
+        if(kg > 550) {
+            System.out.println("Liftul nu poate functiona daca sunt 550kg in acesta.");
+            setDisponibil(false);
+        } else {
+            System.out.println("Liftul cu " + kg + " kg poate pleca!");
+        }
+    }
+
+    public boolean verificareRefizieLift(){
+        if(dataRevizie == null) {
+            return true;
+        }
+        LocalDate acum = LocalDate.now();
+        return dataRevizie.plusYears(1).isBefore(acum);
     }
 
     public void cheama(){
-        stare.stare(this);
+        if(!disponibil){
+            System.out.println("Momentan indisponibil!");
+        } else {
+            stare.stare(this);
+        }
     }
 }
--- Lift/src/ro/real/main/Main.java
+++ Lift/src/ro/real/main/Main.java
@@ -4,18 +4,49 @@ import ro.real.clase.CoboaraState;
 import ro.real.clase.Lift;
 import ro.real.clase.UrcaState;
 
+import java.time.LocalDate;
+
+//
+  // LocalDate introdus doar pentru revizie in constructor, ca atunci cand se deschide un lift sa aiba si
+  // o data de revizie deja stabilita la interval de 1 an.
+//
+
 public class Main {
     public static void main(String[] args) {
-        Lift lift = new Lift();
+
+        Lift lift = new Lift(LocalDate.of(2024,8,3));
         System.out.println("Stare lift: " + lift.getStare().getStare());
 
+        if(lift.verificareRefizieLift()) {
+            System.out.println("Liftul necesita o revizie!");
+            lift.setDisponibil(false);
+        } else {
+            System.out.println("Liftul este in termen!");
+        }
+
+        System.out.println("---------------------------");
+
+
         lift.setStare(new UrcaState());
-        lift.setEtajCurent(0);
         lift.urcaEtaj(3);
         lift.cheama();
         lift.setStare(new CoboaraState());
         lift.coboaraEtaj(2);
         lift.cheama();
+        lift.coboaraEtaj(2);
+        lift.cheama();
+
+        System.out.println("---------------------------");
+        lift.pornireLiftKilograme(400);
+        lift.urcaEtaj(7);
+        lift.cheama();
+
+        System.out.println("---------------------------");
+
+        lift.pornireLiftPersoane(10);
+        lift.urcaEtaj(10);
+        lift.cheama();
+
 
 
         System.out.println("Stare noua: " + lift.getStare().getStare());
